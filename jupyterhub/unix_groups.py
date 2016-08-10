071cf8c7a2b747ad249c9fd8008e3e52e4dd0f4c
Amerterasu <Khoa@Khoas-MacBook-Pro.local>
2016-08-10 13:52:45 -0600

    Added in token vertification to check that token belongs to admin

--- jupyterhub/unix_groups.py
+++ jupyterhub/unix_groups.py
@@ -6,6 +6,17 @@ import sys
 
 class Unix_Groups:
     API_URL = 'http://127.0.0.1:8081/hub/api'
+
+    def tokenAdmin(self, token):
+        rUrl = self.API_URL + '/authorizations/token/' + token
+        r = requests.get(rUrl,
+            headers = {
+                'Authorization' : 'token %s' % token,
+            }
+        )
+        userData = r.json()
+        print(r.json())
+        return userData['admin']
     def getUserData(self, user, token):
         #args:
         # user - the targeted user info
@@ -16,29 +27,45 @@ class Unix_Groups:
             }
         )
         r.raise_for_status()
+        print(r.json())
+        return r.json()
+    def setUserAdmin(self, user, admin):
+        payload = json.dumps({"admin" : admin})
+        userData = self.getUserData(user,self.token)
+
+        r = request.patch(self.API_URL + '/users' + user,
+            headers = {
+                'Authorization' : 'toekn %s' % self.token
+            },
+            data = payload
+        )
+        r.raise_for_status()
         return r.json()
     def __init__(self, user, token, groupList):
         self.token = token
         self.groups = groupList
-        userData = self.getUserData(user, token)
-        if not userData['admin']:
-            print("Given user is not an admin. Some API calls will be unavailable such as changing other users' data")
+        isAdmin = self.tokenAdmin(token)
+        if not isAdmin:
+            print("Given user is not an admin. Can not set admins")
         else:
             for group in groups:
                 members = grp.getgrnam(group).gr_mem
-                print(members)
+                for member in members:
+                    setUserAdmin(member, True)
+
     def update(groupList):
         groups = groupList
         diff = list(set(self.groups) - set(groups))
         for group in groups:
             members = grp.getgrnam(group).gr_mem
             for member in members:
-                user = self.getUserData(member, )
+                setUserAdmin(member, True)
 
 
+#testing
 token = '95182c3a040b4866aa54d25e2dc265ec'
 pay_load = {"admin": False}
 parameters = {"name" : 'guest'}
-user = '/khoa le'
+user = '/guest'
 groups = ['everyone']
 unix = Unix_Groups(user, token, groups)
